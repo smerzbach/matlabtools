1c64bf2e1ca80c7866dbdcaf316860c749c83a52
smerzbach <smerzbach@gmail.com>
2017-02-09 00:43:17 +0100

    make paths absolute again

---
+++ +tb/make_absolute.m
@@ -0,0 +1,34 @@
+% *************************************************************************
+% * Copyright 2017 Sebastian Merzbach
+% *
+% * authors:
+% *  - Sebastian Merzbach <smerzbach@gmail.com>
+% *
+% * file creation date: 2017-02-08
+% *
+% * This file is part of smml.
+% *
+% * smml is free software: you can redistribute it and/or modify it under
+% * the terms of the GNU Lesser General Public License as published by the
+% * Free Software Foundation, either version 3 of the License, or (at your
+% * option) any later version.
+% *
+% * smml is distributed in the hope that it will be useful, but WITHOUT
+% * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+% * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+% * License for more details.
+% *
+% * You should have received a copy of the GNU Lesser General Public
+% * License along with smml.  If not, see <http://www.gnu.org/licenses/>.
+% *
+% *************************************************************************
+% 
+% Make an input path to a file or folder absolute.
+function absolute_path = make_absolute(input)
+    java_file = java.io.File(input);
+    if ~java_file.isAbsolute()
+        java_file = java.io.File(fullfile(pwd, input));
+    end
+    
+    absolute_path = char(java_file.getCanonicalPath());
+end
