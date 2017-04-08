908f0a4501f7041a8f0966d2715473852c83cda7
smerzbach <smerzbach@gmail.com>
2017-04-08 14:08:58 +0200

    cellfun wrapper for nonuniform output arrays

---
+++ misc/cfun.m
@@ -0,0 +1,31 @@
+% *************************************************************************
+% * Copyright 2017 Sebastian Merzbach
+% *
+% * authors:
+% *  - Sebastian Merzbach <smerzbach@gmail.com>
+% *
+% * file creation date: 2017-04-08
+% *
+% * This file is part of smml.
+% *
+% * smml is free software: you can redistribute it and/or modify it under
+% * the terms of the GNU Lesser General Public License as published by the
+% * Free Software Foundation, either version 3 of the License, or (at your
+% * option) any later version.
+% *
+% * smml is distributed in the hope that it will be useful, but WITHOUT
+% * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+% * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+% * License for more details.
+% *
+% * You should have received a copy of the GNU Lesser General Public
+% * License along with smml.  If not, see <http://www.gnu.org/licenses/>.
+% *
+% *************************************************************************
+% 
+% Simple wrapper for cellfun calling it with the annoying UniformOutput
+% argument. Usage is exactly like cellfun otherwise.
+function varargout = cfun(varargin)
+    varargout = cell(nargout, 1);
+    [varargout{:}] = cellfun(varargin{:}, 'UniformOutput', false);
+end
