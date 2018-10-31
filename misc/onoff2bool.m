8d927937268c5a91942f38a6ee3f05d22128f04e
smerzbach <smerzbach@gmail.com>
2018-11-01 00:12:14 +0100

    added missing functions

---
+++ misc/onoff2bool.m
@@ -0,0 +1,34 @@
+% *************************************************************************
+% * Copyright 2018 Sebastian Merzbach
+% *
+% * authors:
+% *  - Sebastian Merzbach <smerzbach@gmail.com>
+% *
+% * file creation date: 2018-07-30
+% *
+% * This file is part of smml.
+% *
+% * smml is free software: you can redistribute it and/or modify it under
+% * the terms of the GNU Lesser General Public License as published by the
+% * Free Software Foundation, either version 3 of the License, or (at your
+% * option) any later version.
+% *
+% * smml is distributed in the hope that it will be useful, but WITHOUT
+% * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+% * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+% * License for more details.
+% *
+% * You should have received a copy of the GNU Lesser General Public
+% * License along with smml.  If not, see <http://www.gnu.org/licenses/>.
+% *
+% *************************************************************************
+%
+% Silly helper for Matlab handle classes, converting 'onf' / 'off' to
+% boolean values.
+function bool = onoff2bool(onoff)
+    if onoff
+        bool = 'true';
+    else
+        bool = 'false';
+    end
+end
