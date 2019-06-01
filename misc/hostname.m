75194336417cc07ebcc078a4a4c24970aa6ac828
smerzbach <smerzbach@gmail.com>
2019-06-01 17:01:19 +0200

    hostname: get or check computer's hostname

---
+++ misc/hostname.m
@@ -0,0 +1,33 @@
+% *************************************************************************
+% * Copyright 2019 Sebastian Merzbach
+% *
+% * authors:
+% *  - Sebastian Merzbach <smerzbach@gmail.com>
+% *
+% * file creation date: 2019-06-01
+% *
+% * This file is part of smml.
+% *
+% * smml is free software: you can redistribute it and/or modify it under
+% * the terms of the GNU Lesser General Public License as published by the
+% * Free Software Foundation, either version 3 of the License, or (at your
+% * option) any later version.
+% *
+% * smml is distributed in the hope that it will be useful, but WITHOUT
+% * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+% * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+% * License for more details.
+% *
+% * You should have received a copy of the GNU Lesser General Public
+% * License along with smml.  If not, see <http://www.gnu.org/licenses/>.
+% *
+% *************************************************************************
+% 
+% Return or check hostname.
+function out = hostname(name)
+    [~, out] = system('hostname');
+    out = strtrim(out);
+    if exist('name', 'var') && ~isempty(name)
+        out = strcmp(out, name);
+    end
+end
